dead691cda7a335f8f2a31c91ee1d0e5db04abf0
Jeeva3112 <78954537+Jeeva3112@users.noreply.github.com>
2024-08-11 12:12:50 +0530

    Update main.tf
    updated mai.tf with basic ec2 creation

--- main.tf
+++ main.tf
@@ -1,18 +1,24 @@
 terraform {
   required_providers {
     aws = {
-      source = "hashicorp/aws"
+      source  = "hashicorp/aws"
       version = "~> 5.0"
     }
   }
 }
 
+# Configure the AWS Provider
 provider "aws" {
   region = "us-east-1"
-  
 }
-resource "aws_instance" "sample" {
-  instance_type = var.instance_type
-  availability_zone = var.availability_zone
+
+resource "aws_instance" "sample-ec2" {
+    ami = "ami-0a0e5d9c7acc336f1"
+    instance_type = "t2.micro"
+    key_name = "jeeva"
+    availability_zone = "us-east-1a"
+    tags = {
+        Name = "tf-ex"
+    }
   
 }
